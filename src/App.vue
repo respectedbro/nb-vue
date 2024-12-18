b3186249785b1b4d30eaf396b490639df92a8bb4
resAsus <respectedbro@yandex.ru>
2024-12-18 17:13:09 +0300

    app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Nik blog</h1>
+  <h2>Vue, React, TD</h2>
+  <my-hello></my-hello>
+</template>
+
+<script setup>
+import MyHello from '@/components/myHello.vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/myHello.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue'
+
+let count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+const updateCount = (e) => {
+  count.value = Number(e.target.value)
+}
+
+</script>
+
+<template>
+  <h1>{{count}}</h1>
+  <button @click="increment">+</button>
+
+  <input @input="updateCount">
+</template>
+
+<style scoped>
+
+
+</style>
