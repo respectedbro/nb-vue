5e447214696895d305f1afaf0a2ef0090d6bc756
NikDm <respectedoff@yandex.ru>
2025-01-10 20:36:14 +0300

    lay header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <script setup></script>
 
 <template>
-  <div class="">
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="logo" class="w-10" />
+        <div>
+          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+          <p class="text-slate-400">Магазин лучших кроссовок</p>
+        </div>
+      </div>
+
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center gap-3 text-gray-500">
+          <img src="/cart.svg" alt="Cart" />
+          <b>1205 руб.</b>
+        </li>
+        <li class="flex items-center gap-3">
+          <img src="/heart.svg" alt="Cart" />
+          <b>Закладки</b>
+        </li>
+        <li class="flex items-center gap-3">
+          <img src="/profile.svg" alt="Cart" />
+          <b>Профиль</b>
+        </li>
+      </ul>
+    </header>
+
     <h1>Nik Blog</h1>
   </div>
 </template>
