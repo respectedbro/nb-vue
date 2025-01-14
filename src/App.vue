079c35bbd6d30c536a62f85e44940898e69aa955
NikDm <respectedoff@yandex.ru>
2025-01-14 22:52:53 +0300

    filters select

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,47 @@
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue'
+
 import Header from '@/components/Header.vue'
 import CardList from '@/components/CardList.vue'
+import axios from 'axios'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: '',
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy,
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get(`https://44ffac03096c71f1.mokky.dev/items`, {
+      params,
+    })
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(fetchItems)
+
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -8,18 +49,29 @@ import CardList from '@/components/CardList.vue'
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
     <div class="p-10">
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-        <div class="relative">
-          <img class="absolute left-3 top-3" src="/search.svg" alt="search" />
-          <input
-            class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
-            placeholder="Поиск..."
-            type="text"
-          />
+        <div class="flex gap-4">
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевле)</option>
+            <option value="-price">По цене (дороже)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-3 top-3" src="/search.svg" alt="search" />
+            <input
+              @change="onChangeSearchInput"
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              placeholder="Поиск..."
+              type="text"
+            />
+          </div>
         </div>
       </div>
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,20 +1,24 @@
-<template>
-  <div class="grid grid-cols-4 gap-5">
-    <Card
-      imageUrl="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="12999"
-      :is-favorite="false"
-      :on-click-add="onClickAdd"
-      :is-added="true"
-    />
-  </div>
-</template>
-
 <script setup>
 import Card from './Card.vue'
+
+defineProps({
+  items: Array,
+})
 
 const onClickAdd = () => {
   alert('Добавить')
 }
 </script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :imageUrl="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
+      :on-click-add="onClickAdd"
+    />
+  </div>
+</template>
