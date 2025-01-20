c6fffad8a1c553072e43758a2b23b09eca389be9
resAsus <respectedbro@yandex.ru>
2025-01-20 18:54:30 +0300

    add Home ^ pages router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup>
-import { computed, onMounted, provide, reactive, ref, watch } from 'vue'
+import { computed, ref, provide } from 'vue'
+import axios from 'axios'
 
 import Drawer from '@/components/Drawer.vue'
 import Header from '@/components/Header.vue'
-import CardList from '@/components/CardList.vue'
-import axios from 'axios'
+import Home from '@/pages/Home.vue'
 
-const items = ref([])
+/*корзина (START)*/
 const cart = ref([])
 const isCreatingOrder = ref(false)
 
-const isLoading = ref(false)
 
 const drawerOpen = ref(false)
 
@@ -57,133 +56,15 @@ const createOrder = async () => {
     isCreatingOrder.value = false
   }
 }
-
-const omClickAddPlus = (item) => {
-  if (!item.isAdded) {
-    addToCart(item)
-  } else {
-    removeFromCart(item)
-  }
-
-  console.log(cart)
-}
-
-const filters = reactive({
-  sortBy: 'title',
-  searchQuery: ''
+/*корзина (END)*/
+provide('cart', {
+  cart,
+  closeDrawer,
+  openDrawer,
+  addToCart,
+  removeFromCart
 })
 
-const onChangeSelect = (event) => {
-  filters.sortBy = event.target.value
-}
-
-const onChangeSearchInput = (event) => {
-  filters.searchQuery = event.target.value
-}
-
-const fetchFavorites = async () => {
-  isLoading.value = true
-  try {
-    const { data: favorites } = await axios.get(`https://44ffac03096c71f1.mokky.dev/favorites`)
-    items.value = items.value.map((item) => {
-      const favorite = favorites.find((favorite) => favorite.parentId === item.id)
-
-      if (!favorite) {
-        return item
-      }
-
-      return {
-        ...item,
-        isFavorite: true,
-        favoriteId: favorite.id
-      }
-    })
-  } catch (err) {
-    console.log(err)
-  } finally {
-    isLoading.value = false
-  }
-}
-
-const addToFavorite = async (item) => {
-  try {
-    if (!item.isFavorite) {
-      const obj = {
-        parentId: item.id
-      }
-
-      item.isFavorite = true
-
-      const { data } = await axios.post(`https://44ffac03096c71f1.mokky.dev/favorites`, obj)
-
-      item.favoriteId = data.id
-    } else {
-      item.isFavorite = false
-
-      await axios.delete(`https://44ffac03096c71f1.mokky.dev/favorites/${item.favoriteId}`)
-      item.favoriteId = null
-    }
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-const fetchItems = async () => {
-  isLoading.value = true
-  try {
-    const params = {
-      sortBy: filters.sortBy
-    }
-
-    if (filters.searchQuery) {
-      params.title = `*${filters.searchQuery}*`
-    }
-
-    const { data } = await axios.get(`https://44ffac03096c71f1.mokky.dev/items`, {
-      params
-    })
-    items.value = data.map((obj) => ({
-      ...obj,
-      isFavorite: false,
-      favoriteId: null,
-      isAdded: false
-    }))
-  } catch (err) {
-    console.log(err)
-  } finally {
-    isLoading.value = false
-  }
-}
-
-onMounted(async () => {
-  const localCart = localStorage.getItem('cart')
-  cart.value = localCart ? JSON.parse(localCart) : []
-
-  await fetchItems()
-  await fetchFavorites()
-
-  items.value = items.value.map((item) => ({
-    ...item,
-    isAdded: cart.value.some((cartItem) => cartItem.id === item.id)
-  }))
-})
-
-watch(filters, fetchItems)
-
-watch(cart, () => {
-  items.value = items.value.map((item) => ({
-    ...item,
-    isAdded: false
-  }))
-})
-
-watch(cart, () => {
-    localStorage.setItem('cart', JSON.stringify(cart.value))
-  },
-  { deep: true }
-)
-
-provide('cart', { cart, closeDrawer, openDrawer, addToCart, removeFromCart })
 </script>
 
 <template>
@@ -198,33 +79,8 @@ provide('cart', { cart, closeDrawer, openDrawer, addToCart, removeFromCart })
     <Header :total-price="totalPrice" @open-drawer="openDrawer" />
 
     <div class="p-10">
-      <div class="flex justify-between items-center">
-        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-        <div class="flex gap-4">
-          <select  @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
-            <option value="name">По названию</option>
-            <option value="price">По цене (дешевле)</option>
-            <option value="-price">По цене (дороже)</option>
-          </select>
-
-          <div class="relative">
-            <img class="absolute left-3 top-3" src="/search.svg" alt="search" />
-            <input
-              @input="onChangeSearchInput"
-              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
-              placeholder="Поиск..."
-              type="text"
-            />
-          </div>
-        </div>
-      </div>
-      <div v-if="isLoading" class="flex justify-center items-center mt-10">
-        <img src="/loader/bouncing-squares.svg" alt="Loading..." class="h-10 w-10" />
-      </div>
-
-      <div v-else class="mt-10">
-        <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="omClickAddPlus" />
-      </div>
+     <router-view>
+     </router-view>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import App from './App.vue'
 
 const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHashHistory()
+})
 
 app.use(autoAnimatePlugin)
 
